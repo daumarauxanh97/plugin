6057262733de0e1593ad42b6e75f7a775b3014f5
daumarauxanh97 <37074701+daumarauxanh97@users.noreply.github.com>
2018-11-11 00:48:34 +0700

    Add files via upload

--- jquery_plugin/plugin.js
+++ jquery_plugin/plugin.js
@@ -1,8 +1,6 @@
 $.fn.inner_float = function() {
 	var child_id=$(this).attr("id");
 	var parent_class=$(this).parent().attr("Class");
-	$(this).css('position','absolute');
-	$(this).parent().css('position','relative');
 	var parent_height=$(this).parent().height();
 	var child_height=$(this).height();
 	var child_top=$(this).offset().top;
@@ -11,18 +9,12 @@ $.fn.inner_float = function() {
 	function myFunction() {
 		var scrollTop=document.documentElement.scrollTop;
 	    if ( scrollTop > child_top && scrollTop < (parent_top+parent_height-child_height)){
-	        // child_top=0;
 	        $("#"+child_id).offset({top:scrollTop});
-	        console.log('1');
 	    } 
 	    else if(scrollTop < child_top) {
-	    	console.log('2');
-	    	// child_top-=10;
 	        $("#"+child_id).offset({top:child_top});
 	    }
 	    else if(scrollTop > (parent_top+parent_height-child_height)) {
-	    	console.log('3');
-	    	// child_top-=10;
 	        $("#"+child_id).offset({top:parent_top+parent_height-child_height});
 	    }
 	}
